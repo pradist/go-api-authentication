40e7b53ebe2c505aa736b97ddab243b9449616cc
Pradist Keawborisut <pradist.ke@gmail.com>
2024-05-09 22:56:44 +0700

    feat: move secretKey to global scope

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -7,11 +7,10 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+var secretKey = []byte(os.Getenv("SECRET_KEY"))
+
 // GenerateToken generates a JWT token with the user ID as part of the claims
 func GenerateToken(userID uint) (string, error) {
-
-	secretKey := []byte(os.Getenv("SECRET_KEY"))
-
 	claims := jwt.MapClaims{}
 	claims["user_id"] = userID
 	claims["exp"] = time.Now().Add(time.Hour).Unix() // Token valid for 1 hour
